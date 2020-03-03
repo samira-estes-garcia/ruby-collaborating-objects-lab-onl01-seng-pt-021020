c9ac7ebf77f49c3acb8dee670f529d1e6c41cc46
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-03 23:45:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,8 +18,16 @@ class Song
  end
   
   def self.new_by_filename(file_name)
-    self.name = file_name.split(" - ")[0]
-    self.artist.name = file_name.split(" - ")[1]
+    self.name = file_name.split(" - ")[1]
+    self.artist.name = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
   
 end
