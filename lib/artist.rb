e7a00a055b113fdd6382251fc5697b5f78b07c03
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-03 23:00:44 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+class Artist 
+  
+  attr_accessor :name
+  
+  @@all = [ ]
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def songs 
+    Song.all.select { |song| song.artist == self}
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def self.find_or_create_by_name(name)
+    
+    
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def print_songs
+    puts songs
+  end
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song 
+  
+  attr_accessor :name, :artist 
+  
+  @@all = [ ]
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.new_by_filename(filename)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
+  end
+  
+
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+
+  
+  
+  
+end
