c68ff5d95d90199b8c7f2d182d9b37664285f135
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-03 23:26:44 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -16,9 +16,9 @@ class MP3Importer
   
   def import
     @filenames.each do |filename|
-      artist_name = filename.split(" - ")[2]
+      artist_name = filename.split(" - ")[1]
       Artist.find_or_create_by_name(artist_name)
-      song = filename.split(" - ")[1]
+      song = filename.split(" - ")[0]
       Artist.add_song(song)
     end
   end
